46e4389ed662b0f8ab7fa706c614591c5cd1f8c8
Jon <jon-doucette@hotmail.com>
2018-03-14 11:08:01 -0700

    display_text updated to take multiple messages

--- Organizational/display_text.m
+++ Organizational/display_text.m
@@ -1,5 +1,6 @@
 function new_msg = display_text( msg, line_width, break_char, is_centered, addspace )
-%DISPLAY_TEXT Displays the (potentially very long) string msg such that the
+%display_text( msg, line_width, break_char, is_centered, addspace )
+% Displays the (potentially very long) string msg such that the
 % maximum line length width is line_width. Optionally the message can be
 % padded with strings of break_char's or be centered.
 
@@ -11,13 +12,29 @@ elseif isscalar(addspace)
     else                addspace	=   [ 0, addspace ];
     end
 end
+if nargin < 4 || isempty(is_centered)
+    is_centered = true;
+end
+if nargin < 3 || isempty(break_char)
+    break_char = '%';
+end
 
-msg	=	format_text( msg, line_width, break_char, is_centered );
+if ~iscell(msg); msg = {msg}; end
 
 fprintf( repmat( '\n', 1, round(abs(addspace(1))) ) );
 
-for ii = 1:length(msg)
-    fprintf(msg{ii});
+for ii = 1:numel(msg)
+    msg_formatted	=	format_text( ...
+        msg{ii}, line_width, break_char, is_centered );
+    
+    if ii == 1; fprintf(msg_formatted{1}); end
+    
+    for jj = 2:numel(msg_formatted)-1
+        fprintf(msg_formatted{jj});
+    end
+    
+    if ii == numel(msg); fprintf(msg_formatted{end}); end
+
 end
 
 fprintf( repmat( '\n', 1, round(abs(addspace(2))) ) );
