3d7164e678852535ba473a55cf3fdf7f9eeb1605
Jonathan Doucette <jdoucette@phas.ubc.ca>
2018-07-04 14:34:25 -0700

    angle3D fixes

--- Geometry/angle3D.m
+++ Geometry/angle3D.m
@@ -5,11 +5,12 @@ function angle = angle3D( vec1, vec2, dim, isDegrees )
 % NOTE: angle is returned in radians, unless isDegrees is true
 
 if nargin < 3
-    dim	=   find( size(vec1) == 3, true, 'first' );
+    dim =   find( size(vec1) == 3, true, 'first' );
 end
 
-angle	=	atan2(  norm( cross( vec1, vec2, dim ) ),  ...
-                    dot( vec1, vec2, dim )	);
+vecnorm = @(x,dim) sqrt(sum(x.*x, dim));
+angle = atan2( vecnorm( cross( vec1, vec2, dim ), dim ), ...
+               dot( vec1, vec2, dim ) );
 
 if nargin > 3 && isDegrees
     angle = 180/pi * angle;
