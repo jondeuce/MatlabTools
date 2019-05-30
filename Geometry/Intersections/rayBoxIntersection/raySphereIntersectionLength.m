c3be777e4877d3d4786b95f65d0d4bd0514d7875
Jonathan Doucette <jdoucette@phas.ubc.ca>
2019-05-29 17:33:30 -0700

    Generate cylinders within a sphere isotropically

---
+++ Geometry/Intersections/rayBoxIntersection/raySphereIntersectionLength.m
@@ -0,0 +1,18 @@
+function [ L ] = raySphereIntersectionLength( Origins, Directions, SphereRadius, SphereCenter )
+%RAYBOXINTERSECTIONLENGTH raySphereIntersectionLength( Origins, Directions, SphereRadius, SphereCenter )
+
+[tmin, tmax, ~, ~] = raySphereIntersection( Origins, Directions, SphereRadius, SphereCenter );
+L = tmax - tmin;
+
+end
+
+
+
+
+
+
+
+
+
+
+
